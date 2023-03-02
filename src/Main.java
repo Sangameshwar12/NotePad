46a63c3e7bfdc4b7f4b1023ad9df70fe9edc25f0
Sangameshwar12 <95075051+Sangameshwar12@users.noreply.github.com>
2023-03-03 02:03:55 +0530

    Update Main.java
    I have added features in this project like view button etc.

--- src/Main.java
+++ src/Main.java
@@ -1,125 +1,219 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
-import java.awt.print.PrinterException;
+import java.io.File;
+public class notepad extends KeyAdapter  implements ActionListener, KeyListener
+{
 
-class note extends JFrame implements ActionListener{
+    static int active =0;
+    static int fsize=17;
 
-    // frame declaration
-    JFrame f;
+    JFrame frame1;
+    JMenuBar npMenuBar;
+    JMenu file, edit, format, view;
+    JMenuItem newdoc, opendoc, exit, savedoc, saveasdoc, copydoc, pastedoc, remdoc,  fontfamily, fontstyle, fontsize, status;
+    JTextArea maintext;
+    JTextField title;
+    Font font1;
+    JPanel bottom;
+    JLabel details, pastecopydoc;
+    JList familylist, stylelist, sizelist;
+    JScrollPane sb;
 
-    JTextArea t; // textarea declaration
+    String familyvalue[]={"Agency FB","Antiqua","Architect","Arial","Calibri","Comic Sans","Courier","Cursive","Impact","Serif"};
+    String sizevalue[]={"5","10","15","20","25","30","35","40","45","50","55","60","65","70"};
+    int [] stylevalue={ Font.PLAIN, Font.BOLD, Font.ITALIC };
+    String [] stylevalues={ "PLAIN", "BOLD", "ITALIC" };
+    String ffamily, fsizestr, fstylestr;
+    int fstyle;
+    int cl;
+    int linecount;
+    String tle ;
+    String topicstitle = "";
+    JScrollPane sp;
 
+    notepad(){
 
-    // constructor
-    note(){
-        // initializing the frame
-        f = new JFrame("notepad");
+        frame1 = new JFrame("Notepad Fast");
 
-        // initializing the textarea
-        t = new JTextArea();
+        font1=new Font("Arial",Font.PLAIN,17);
 
-        // create a menubar
-        JMenuBar menuBar = new JMenuBar();
+        bottom = new JPanel();
+        details = new JLabel();
+        pastecopydoc = new JLabel();
 
-        // creating the file menu
-        JMenu file = new JMenu("File");
-        // creating the option for files
-        JMenuItem f1 = new JMenuItem("New");
-        JMenuItem f2 = new JMenuItem("Save");
-        JMenuItem f3 = new JMenuItem("Open");
-        JMenuItem f4 = new JMenuItem("Print");
-
-        // adding the actionlistener to files;
-        f1.addActionListener(this);
-        f2.addActionListener(this);
-        f3.addActionListener(this);
-        f4.addActionListener(this);
-
-        // adding the options to the file menu
-        file.add(f1);
-        file.add(f2);
-        file.add(f3);
-        file.add(f4);
-
-        // creating the edit menu
-        JMenu edit = new JMenu("Edit");
-        // creating the option for files
-        JMenuItem e1 = new JMenuItem("Cut");
-        JMenuItem e2 = new JMenuItem("Copy");
-        JMenuItem e3 = new JMenuItem("Paste");
-
-        // adding the actionlistener to files;
-        e1.addActionListener(this);
-        e2.addActionListener(this);
-        e3.addActionListener(this);
-
-        // adding the options to the file menu
-        edit.add(e1);
-        edit.add(e2);
-        edit.add(e3);
-
-        // creating the close button
-        JMenuItem close = new JMenuItem("Close");
-        close.addActionListener(this);
-
-        // adding menus to the menubar
-        menuBar.add(file);
-        menuBar.add(edit);
-        menuBar.add(close);
-
-        f.setJMenuBar(menuBar);
-        f.add(t);
-        f.setSize(1280, 720);
-        f.setVisible(true);
+        familylist = new JList(familyvalue);
+        stylelist = new JList(stylevalues);
+        sizelist = new JList(sizevalue);
 
 
+        familylist.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        sizelist.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        stylelist.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
+        bottom.add(details);
 
+        maintext = new JTextArea();
+
+        sp=new JScrollPane(maintext);
+        title = new JTextField(100);
+
+        sb = new JScrollPane(maintext);
+
+        maintext.setMargin( new Insets(5,5,5,5) );
+
+        maintext.setFont(font1);
+        frame1.add(maintext);
+
+        npMenuBar = new JMenuBar();
+
+        file = new JMenu("File");
+        edit = new JMenu("Edit");
+        format = new JMenu("Format");
+        view = new JMenu("View");
+
+        newdoc = new JMenuItem("New Document");
+        opendoc = new JMenuItem("Open Document");
+        savedoc = new JMenuItem("Save Document");
+        saveasdoc = new JMenuItem("Save As Document");
+        exit = new JMenuItem("Exit");
+
+        copydoc = new JMenuItem("Copy Document");
+        remdoc = new JMenuItem("Remove Document");
+        pastedoc = new JMenuItem("Paste Document");
+
+        fontfamily = new JMenuItem("Set Font Family");
+        fontstyle = new JMenuItem("Set Font Style");
+        fontsize = new JMenuItem("Set Font Size");
+        status = new JMenuItem("Status");
+
+        file.add(newdoc);
+        file.add(opendoc);
+        file.add(savedoc);
+        file.add(saveasdoc);
+        file.add(exit);
+
+        edit.add(copydoc);
+        edit.add(pastedoc);
+        edit.add(remdoc);
+
+        format.add(fontfamily);
+        format.add(fontstyle);
+        format.add(fontsize);
+
+        view.add(status);
+
+        npMenuBar.add(file);
+        npMenuBar.add(edit);
+        npMenuBar.add(format);
+        npMenuBar.add(view);
+
+        frame1.setJMenuBar(npMenuBar);
+        frame1.add(bottom, BorderLayout.SOUTH);
+
+        newdoc.addActionListener(this);
+        copydoc.addActionListener(this);
+        pastedoc.addActionListener(this);
+        remdoc.addActionListener(this);
+        status.addActionListener(this);
+        savedoc.addActionListener(this);
+        saveasdoc.addActionListener(this);
+
+        fontfamily.addActionListener(this);
+        fontsize.addActionListener(this);
+        fontstyle.addActionListener(this);
+
+        exit.addActionListener(this);
+
+        maintext.addKeyListener(this);
+
+        frame1.setSize(600,600);
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame1.setLocationRelativeTo(null);
+        frame1.setVisible(true);
     }
-
-
-
-    // to add the functionlities;
-    public void actionPerformed(ActionEvent e){
-
-        // extracting the command into a string
-        String s = e.getActionCommand();
-
-        switch (s){
-            case "New":
-                t.setText("");
-                break;
-            case "Save":
-
-                break;
-            case "Open":
-                break;
-            case "Print":
-                try {
-                    t.print();
-                } catch (PrinterException ex) {
-                    throw new RuntimeException(ex);
+    public void actionPerformed(ActionEvent ae)
+    {
+        if(ae.getSource()== newdoc)
+        {
+            frame1.setTitle("New Document.txt");
+            maintext.setText("");
+        }
+        else if (ae.getSource()== copydoc)
+        {
+            String texts= maintext.getText();
+            pastecopydoc.setText(texts);
+            JOptionPane.showMessageDialog(null, "Copy Text "+texts);
+        }
+        else if(ae.getSource()== remdoc)
+        {
+            maintext.setText("");
+            JOptionPane.showMessageDialog(null, "Removed");
+        }
+        else if (ae.getSource() == pastedoc)
+        {
+            if(maintext.getText().length() != 0)
+            {
+                maintext.setText(maintext.getText());
+            }
+            else
+            {
+                maintext.setText(pastecopydoc.getText());
+            }
+        }
+        else if(ae.getSource()== status)
+        {
+            try{
+                if(active ==0)
+                {
+                    File f = new File(tle+".txt");
+                    details.setText("Size: "+f.length());
                 }
-                break;
-            case "Cut":
-                t.cut();
-                break;
-            case "Copy":
-                t.copy();
-                break;
-            case "Paste":
-                t.paste();
-                break;
-            case "Close":
-                t.setVisible(false);
-                break;
+            }
+            catch (Exception e)
+            {
 
+            }
+        }
+        else if (ae.getSource()== fontfamily)
+        {
 
+            JOptionPane.showConfirmDialog(null, familylist, "Choose Font Family", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+            ffamily=String.valueOf(familylist.getSelectedValue());
+            font1=new Font(ffamily,fstyle,fsize);
+            maintext.setFont(font1);
+        }
+        else if (ae.getSource()== fontstyle)
+        {
+
+            JOptionPane.showConfirmDialog(null, stylelist, "Choose Font Style", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+            fstyle=stylevalue[stylelist.getSelectedIndex()];
+            font1=new Font(ffamily,fstyle,fsize);
+            maintext.setFont(font1);
+        }
+        else if (ae.getSource()== fontsize)
+        {
+
+            JOptionPane.showConfirmDialog(null, sizelist, "Choose Font Size", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+            fsizestr=String.valueOf(sizelist.getSelectedValue());
+            fsize =Integer.parseInt(fsizestr);
+            font1=new Font(ffamily,fstyle,fsize);
+            maintext.setFont(font1);
+        }
+        else if(ae.getSource()==exit)
+        {
+            frame1.dispose();
         }
 
     }
-
-    public static void main(String args[]){
-        note n = new note();
+    public void keyTyped(KeyEvent ke){
+        cl= maintext.getText().length();
+        linecount = maintext.getLineCount();
+        details.setText("Length: "+cl+" Line: "+linecount);
+    }
+    public static void main(String ar[])
+    {
+        new notepad();
     }
 }
+
